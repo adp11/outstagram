8b10c0ff615d16e2292dd225677f2f582cd045a9
Andrew Pham <locpp2001@gmail.com>
2022-04-27 20:21:59 -0400

    added Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,22 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
 
 // App's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDb-Fpaj-p4d2r9J2QpOyeJX-e9EBk1y6U',
-  authDomain: 'superchat-905f2.firebaseapp.com',
-  projectId: 'superchat-905f2',
-  storageBucket: 'superchat-905f2.appspot.com',
-  messagingSenderId: '1064715598950',
-  appId: '1:1064715598950:web:fcbc8ae42d8042d88fc1a7',
+  apiKey: "AIzaSyDb-Fpaj-p4d2r9J2QpOyeJX-e9EBk1y6U",
+  authDomain: "superchat-905f2.firebaseapp.com",
+  databaseURL: "https://superchat-905f2-default-rtdb.firebaseio.com",
+  projectId: "superchat-905f2",
+  storageBucket: "superchat-905f2.appspot.com",
+  messagingSenderId: "1064715598950",
+  appId: "1:1064715598950:web:fcbc8ae42d8042d88fc1a7",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
+
+// Set Firebase auth
+const auth = getAuth(app);
+connectAuthEmulator(auth, "http://localhost:9099");
+
+export { app, auth };
