23e3f91d8aea1efdf3d503d3394188425e9b203e
Andrew Pham <locpp2001@gmail.com>
2022-05-27 13:44:46 +0700

    added fnc computeHowLongAgo()

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,10 @@
-import {
-  differenceInMinutes, differenceInHours, differenceInDays, differenceInWeeks, differenceInMonths, differenceInYears,
-} from "date-fns";
-// import differenceInHours from "date-fns/differenceInHours";
+import { differenceInMinutes } from "date-fns";
+
+const YEAR_IN_MIN = 1440 * 365;
+const MONTH_IN_MIN = 1440 * 30;
+const WEEK_IN_MIN = 1440 * 7;
+const DAY_IN_MIN = 1440;
+const HOUR_IN_MIN = 60;
 
 function capitalizeFirebaseAuthError(message) {
   const words = message.split("-");
@@ -9,90 +12,54 @@ function capitalizeFirebaseAuthError(message) {
   return words.map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
 }
 
-function swap(items, leftIndex, rightIndex) {
-  const temp = items[leftIndex];
-  items[leftIndex] = items[rightIndex];
-  items[rightIndex] = temp;
-}
-
-function partition(items, left, right) {
-  const pivot = items[Math.floor((right + left) / 2)]; // middle element
-  let i = left; // left pointer
-  let j = right; // right pointer
-  while (i <= j) {
-    while (items[i].creationTime.seconds < pivot.creationTime.seconds) {
-      i++;
-    }
-    while (items[j].creationTime.seconds > pivot.creationTime.seconds) {
-      j--;
-    }
-    if (i <= j) {
-      swap(items, i, j); // swapping two elements
-      i++;
-      j--;
-    }
-  }
-  return i;
-}
-
-function quickSort(items, left, right) {
-  let index;
-  if (items.length > 1) {
-    index = partition(items, left, right); // index returned from partition
-    if (left < index - 1) { // more elements on the left side of the pivot
-      quickSort(items, left, index - 1);
-    }
-    if (index < right) { // more elements on the right side of the pivot
-      quickSort(items, index, right);
-    }
-  }
-  return items;
-}
-
-function locationOf(element, array, start, end) {
-  start = start || 0;
-  end = end || array.length;
-  const pivot = parseInt(start + (end - start) / 2, 10);
-  if (end - start <= 1 || array[pivot].creationTime.seconds === element.creationTime.seconds) return pivot;
-  if (array[pivot].creationTime.seconds < element.creationTime.seconds) {
-    return locationOf(element, array, pivot, end);
-  }
-  return locationOf(element, array, start, pivot);
-}
-
-function insert(element, array) {
-  const targetIndex = locationOf(element, array);
-  if (targetIndex === 0) {
-    array.splice(targetIndex, 0, element);
-  } else {
-    array.splice(targetIndex + 1, 0, element);
-  }
-  return array;
-}
-
-function computeHowLongAgo(unixTime, abbreviation) {
-  let result = differenceInYears(new Date(), new Date(unixTime * 1000) > 0);
-  if (result === 0) {
-    result = differenceInMonths(new Date(), new Date(unixTime * 1000) > 0);
-    if (result === 0) {
-      result = differenceInWeeks(new Date(), new Date(unixTime * 1000) > 0);
-      if (result === 0) {
-        result = differenceInDays(new Date(), new Date(unixTime * 1000) > 0);
-        if (result === 0) {
-          result = differenceInDays(new Date(), new Date(unixTime * 1000) > 0);
-          if (result === 0) {
-            result = differenceInHours(new Date(), new Date(unixTime * 1000) > 0);
-            if (result === 0) {
-              result = differenceInMinutes(new Date(), new Date(unixTime * 1000) > 0);
-              if (result === 0) {
-                result = "JUST NOW";
-              }
+function computeHowLongAgo(unixTime, abbreviation = true) {
+  const diff = differenceInMinutes(new Date(), new Date(unixTime * 1000));
+  let result = Math.floor(diff / YEAR_IN_MIN);
+  if (result < 1) {
+    result = Math.floor(diff / MONTH_IN_MIN);
+    if (result < 1) {
+      result = Math.floor(diff / WEEK_IN_MIN);
+      if (result < 1) {
+        result = Math.floor(diff / DAY_IN_MIN);
+        if (result < 1) {
+          result = Math.floor(diff / HOUR_IN_MIN);
+          if (result < 1) {
+            result = diff;
+            if (result < 1) {
+              return (abbreviation ? "just now" : "JUST NOW");
             }
+            return (abbreviation ? `${result}m` : result > 1 ? `${result} MINUTES AGO` : `${result} MINUTE AGO`);
           }
+          return (abbreviation ? `${result}h` : result > 1 ? `${result} HOURS AGO` : `${result} HOUR AGO`);
         }
+        return (abbreviation ? `${result}d` : result > 1 ? `${result} DAYS AGO` : `${result} DAY AGO`);
       }
+      return (abbreviation ? `${result}w` : result > 1 ? `${result} WEEKS AGO` : `${result} WEEK AGO`);
     }
+    return (abbreviation ? `${result}m` : result > 1 ? `${result} MONTHS AGO` : `${result} MONTH AGO`);
+  }
+  return (abbreviation ? `${result}y` : result > 1 ? `${result} YEARS AGO` : `${result} YEAR AGO`);
+}
+
+// find the location to insert a key into a sorted descending list
+function binarySearch(arr, key, start, end) {
+  if (start > end) return start;
+
+  const mid = Math.floor((start + end) / 2);
+  if (arr[mid].creationTime.seconds === key.creationTime.seconds) return mid;
+  if (arr[mid].creationTime.seconds > key.creationTime.seconds) return binarySearch(arr, key, mid + 1, end);
+  return binarySearch(arr, key, start, mid - 1);
+}
+
+function insert(arr, element) {
+  if (arr.length === 0) {
+    arr.push(element);
+  } else {
+    const pos = binarySearch(arr, element, 0, arr.length - 1);
+    arr.splice(pos, 0, element);
   }
 }
 
-export { capitalizeFirebaseAuthError, quickSort, insert };
+export {
+  capitalizeFirebaseAuthError, insert, computeHowLongAgo,
+};
