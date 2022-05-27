59164eef1dfcacb36261de845e697d7799e434a6
Andrew Pham <locpp2001@gmail.com>
2022-05-27 19:28:35 +0700

    added red heart liked, fixed editProfile 3 fields bug, removed follow button beside username in fullpost, added links to posts'info

--- src/components/FullPost.js
+++ src/components/FullPost.js
@@ -26,6 +26,7 @@ function FullPost() {
 
   // Conditional rendering
   const [componentVars, setComponentVars] = useState({
+    authorId: "",
     authorUsername: "",
     authorPhotoURL: "",
     postPictureURL: "",
@@ -38,16 +39,22 @@ function FullPost() {
   });
   const textareaRef = useRef();
 
-  function handleCloseFullPost() {
+  function handleCloseFullPost(redirect = true) {
     setIsFullPostActive(false);
     if (beforeFullPost.selfProfile) {
-      navigate(`/${userData.uid}`);
+      if (redirect) {
+        navigate(`/${userData.uid}`);
+      }
       setFullPostInfo(null);
     } else if (beforeFullPost.visitedProfile) {
-      navigate(`/${visitedUserData.uid}`);
+      if (redirect) {
+        navigate(`/${visitedUserData.uid}`);
+      }
       setFullPostInfo(null);
     } else {
-      navigate("/");
+      if (redirect) {
+        navigate("/");
+      }
       setFullPostIndex(null);
     }
 
@@ -116,7 +123,7 @@ function FullPost() {
       const postRef = doc(db, `users/${postInfo.authorId}/posts/${postInfo.postId}`);
       const cmtId = uniqid();
       newComments = postInfo.comments.concat({
-        sourceId: getAuth().currentUser.uid,
+        sourceId: `uid_${getAuth().currentUser.uid}`,
         sourcePhotoURL: userData.photoURL,
         sourceUsername: userData.username,
         sourceComment: postComments[postInfo.postId],
@@ -152,7 +159,7 @@ function FullPost() {
       postInfo = fullPostInfo;
     }
     const postRef = doc(db, `users/${postInfo.authorId}/posts/${postInfo.postId}`);
-    const targetIndex = postInfo.likes.findIndex((like) => like.sourceId === getAuth().currentUser.uid);
+    const targetIndex = postInfo.likes.findIndex((like) => like.sourceId === `uid_${getAuth().currentUser.uid}`);
     if (targetIndex !== -1) {
       newLikes = postInfo.likes.filter((like, idx) => idx !== targetIndex);
       await updateDoc(postRef, {
@@ -165,7 +172,7 @@ function FullPost() {
       }
     } else {
       newLikes = postInfo.likes.concat({
-        sourceId: getAuth().currentUser.uid,
+        sourceId: `uid_${getAuth().currentUser.uid}`,
         sourcePhotoURL: userData.photoURL,
         sourceUsername: userData.username,
         sourceDisplayname: userData.displayName,
@@ -196,6 +203,7 @@ function FullPost() {
     console.log("componentVars triggered");
     if (beforeFullPost.newsfeed && fullPostIndex !== null) {
       setComponentVars({
+        authorId: newsfeed[fullPostIndex].authorId,
         authorUsername: newsfeed[fullPostIndex].authorUsername,
         authorPhotoURL: newsfeed[fullPostIndex].authorPhotoURL,
         postPictureURL: newsfeed[fullPostIndex].imageURL,
@@ -208,6 +216,7 @@ function FullPost() {
       });
     } else if ((beforeFullPost.selfProfile || beforeFullPost.visitedProfile) && fullPostInfo !== null) {
       setComponentVars({
+        authorId: fullPostInfo.authorId,
         authorUsername: fullPostInfo.authorUsername,
         authorPhotoURL: fullPostInfo.authorPhotoURL,
         postPictureURL: fullPostInfo.imageURL,
@@ -222,8 +231,19 @@ function FullPost() {
   }, [fullPostInfo, newsfeed[fullPostIndex]]);
 
   const {
-    authorUsername, authorPhotoURL, postPictureURL, postCaption, postCreationTime, postCmts, postLikes, postId, fromWhich,
+    authorId, authorUsername, authorPhotoURL, postPictureURL, postCaption, postCreationTime, postCmts, postLikes, postId, fromWhich,
   } = componentVars;
+
+  async function handleVisitProfile(uid) {
+    handleCloseFullPost(false); // prevent nagivate() 2 times
+    console.log("visiting profile", uid);
+    const docRef = doc(db, `users/${uid}`);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      navigate(`/${uid}`);
+      setVisitedUserData(docSnap.data());
+    }
+  }
 
   return (
     <div className="FullPost">
@@ -242,28 +262,28 @@ function FullPost() {
 
         <div className="post-info">
           <div className="user-profile">
-            <img className="user-avatar" src={authorPhotoURL} alt="" style={{ marginRight: "15px" }} />
-            <span className="username bold medium">{authorUsername}</span>
-            <span className="medium bold" style={{ color: "#0095f6", marginLeft: "10px" }}>Following</span>
+            <img className="user-avatar" src={authorPhotoURL} alt="" style={{ marginRight: "15px" }} onClick={() => { handleVisitProfile(authorId); }} />
+            <span className="username bold medium" onClick={() => { handleVisitProfile(authorId); }}>{authorUsername}</span>
+            {/* <span className="medium bold" style={{ color: "#0095f6", marginLeft: "10px" }}>Following</span> */}
           </div>
           <div className="comment-section">
             {postCaption && (
             <div className="post-caption">
-              <img className="user-avatar" src={authorPhotoURL} alt="" style={{ marginRight: "15px" }} />
+              <img className="user-avatar" src={authorPhotoURL} alt="" style={{ marginRight: "15px" }} onClick={() => { handleVisitProfile(authorId); }} />
               <div>
-                <span className="username bold medium">{authorUsername}</span>
+                <span className="username bold medium" onClick={() => { handleVisitProfile(authorId); }}>{authorUsername}</span>
                 {" "}
                 <span className="medium">{postCaption}</span>
                 <small style={{ display: "block", marginTop: "10px" }} className="grey small">{postCreationTime}</small>
               </div>
             </div>
             )}
-
+            {/* .NewsfeedPost .user-avatar:hover, .NewsfeedPost .username:hover, .FullPost .username:hover, .FullPost .user-avatar:hover */}
             {postCmts.map((comment) => (
               <div className="post-comment" key={comment.sourceCommentTime}>
-                <img src={comment.sourcePhotoURL} alt="" className="user-avatar" style={{ marginRight: "15px" }} />
+                <img src={comment.sourcePhotoURL} alt="" className="user-avatar" style={{ marginRight: "15px" }} onClick={() => { handleVisitProfile(comment.sourceId); }} />
                 <div>
-                  <span className="username bold medium">
+                  <span className="username bold medium" onClick={() => { handleVisitProfile(comment.sourceId); }}>
                     {comment.sourceUsername}
                   </span>
                   {" "}
@@ -276,9 +296,11 @@ function FullPost() {
           </div>
 
           <div style={{ padding: "10px 0", display: "flex", gap: "20px" }} className="post-btns">
-            <svg onClick={handleLikePost} className="like" color="#262626" fill="#262626" height="24" width="24">
-              <path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z" />
-            </svg>
+            {(postLikes.findIndex((like) => `${like.sourceId}` === userData.uid) === -1) ? (
+              <svg onClick={handleLikePost} color="#262626" fill="#262626" height="24" width="24">
+                <path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z" />
+              </svg>
+            ) : <svg onClick={handleLikePost} color="#ed4956" fill="#ed4956" height="24" role="img" viewBox="0 0 48 48" width="24"><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z" /></svg>}
 
             <svg onClick={() => { textareaRef.current.focus(); }} className="comment" color="black" fill="#8e8e8e" height="24" viewBox="0 0 24 24" width="24">
               <path d="M20.656 17.008a9.993 9.993 0 10-3.59 3.615L22 22z" fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2" />
@@ -294,7 +316,7 @@ function FullPost() {
             </svg>
           </div>
 
-          {postLikes.length > 0 ? (
+          {postLikes.length > 10 ? (
             <div className="post-likes medium">
               Liked by
               {" "}
