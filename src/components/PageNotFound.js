95e5e92c1382c89ded016583a51f6db6ec0683d4
Andrew Pham <locpp2001@gmail.com>
2022-06-07 14:20:30 +0700

    added _redirects for react routing in SPA and fixed PageNotFound

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import UserContext from "./Contexts/UserContext";
 
 function PageNotFound() {
+  const { darkMode } = useContext(UserContext);
+
   return (
-    <div className="PageNotFound" style={{ display: "grid", placeItems: "center", height: "calc(100vh - 90px)" }}>
+    <div className="PageNotFound" style={{ display: "grid", placeItems: "center", height: "100vh", backgroundColor: darkMode ? "#000000" : "#fff" }}>
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <img src={`${window.location.origin}/images/page404.jpg`} alt="Page Not Found" style={{ width: "250px", height: "auto" }} />
+        <img src={`${window.location.origin}/images/${darkMode ? "page404-dark.jpg" : "page404-light.jpg"}`} alt="Page Not Found" style={{ width: "250px", height: "auto" }} />
         <h4>Sorry, this page isn't available.</h4>
         <p className="grey">
           The link you followed may be broken, or the page may have been removed.
